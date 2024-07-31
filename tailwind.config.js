0ad0d9731fed1c4e7729f81286108a1b21e14ebb
Ronald Huggler <ronald@huggler.com.br>
2024-07-31 00:56:53 -0300

    Setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          700: "#C47F17",
+          500: "#DBAC2C",
+          300: "#F1E9C9",
+        },
+        purple: {
+          700: "#4B2995",
+          500: "#8047F8",
+          300: "#EBE4F9",
+        },
+        brown: {
+          900: "#272221",
+          800: "#403937",
+          700: "#574F4D",
+          600: "#8D8686",
+          500: "#D7D5D5",
+          400: "#E6E5E5",
+          300: "#EDEDED",
+          200: "#F3F2F2",
+          100: "#FAFAFA",
+        }
+      },
+      fontFamily: {
+        title: ['Baloo 2', 'sans'],
+        roboto: ['Roboto', 'sans']
+      }
+    },
   },
   plugins: [],
 }
