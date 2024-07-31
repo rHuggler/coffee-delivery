2b22d412a63e4fb84ba4e091be9db5be9f44409f
Ronald Huggler <ronald@huggler.com.br>
2024-07-31 18:00:21 -0300

    Create Header HMTL

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
 import coffeeLogo from "../assets/coffee-logo.svg";
 
 export function Header() {
 	return (
 		<header>
 			<img src={coffeeLogo} alt="Coffee delivery logo" />
+			<nav>
+				<MapPin size={24} weight="fill" />
+				<ShoppingCart size={24} weight="fill" />
+			</nav>
 		</header>
 	);
 }
