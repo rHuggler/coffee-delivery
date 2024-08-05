be0116af6a26d065e735c7aafd7f47e729d69b6d
Ronald Huggler <ronald@huggler.com.br>
2024-08-05 19:06:43 -0300

    Create checkout page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import { Checkout } from "./pages/Checkout";
 import { Home } from "./pages/Home";
+import { DefaultLayout } from "./pages/layouts/DefaultLayout";
 import { HeroLayout } from "./pages/layouts/HeroLayout";
 
 export function Router() {
@@ -7,6 +9,9 @@ export function Router() {
 		<Routes>
 			<Route path="/" element={<HeroLayout />}>
 				<Route path="/" element={<Home />} />
+			</Route>
+			<Route path="/checkout" element={<DefaultLayout />}>
+				<Route path="/checkout" element={<Checkout />} />
 			</Route>
 		</Routes>
 	);
