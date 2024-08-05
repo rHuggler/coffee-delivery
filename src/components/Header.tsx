ae6193f35cc9b3a92edcef345245cb439ce407e0
Ronald Huggler <ronald@huggler.com.br>
2024-08-05 19:07:07 -0300

    Add navlinks to home and checkout in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
 import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { NavLink } from "react-router-dom";
 import coffeeLogo from "../assets/coffee-logo.svg";
 
 export function Header() {
 	return (
 		<header className="flex justify-between items-center py-8">
-			<img src={coffeeLogo} alt="Coffee delivery logo" />
+			<NavLink to="/" title="Home">
+				<img src={coffeeLogo} alt="Coffee delivery logo" />
+			</NavLink>
 			<nav className="flex gap-3">
-				<div className="flex gap-1 bg-purple-300 p-2 rounded-md text-purple-500">
+				<div className="flex gap-1 bg-purple-300 p-2 rounded-md text-purple-500 cursor-pointer">
 					<MapPin size={22} weight="fill" />
 					<span className="text-purple-700">Location</span>
 				</div>
-				<div className="bg-yellow-300 p-2 rounded-md text-yellow-700">
-					<ShoppingCart size={22} weight="fill" />
-				</div>
+				<NavLink to="/checkout" title="Checkout">
+					<div className="bg-yellow-300 p-2 rounded-md text-yellow-700 cursor-pointer">
+						<ShoppingCart size={22} weight="fill" />
+					</div>
+				</NavLink>
 			</nav>
 		</header>
 	);
