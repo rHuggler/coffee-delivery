f3a0025614b5ea562b804142c0f1c53e8562b898
Ronald Huggler <ronald@huggler.com.br>
2024-08-05 18:59:40 -0300

    Add color to header buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ export function Header() {
 		<header className="flex justify-between items-center py-8">
 			<img src={coffeeLogo} alt="Coffee delivery logo" />
 			<nav className="flex gap-3">
-				<div className="flex bg-purple-300 p-2 rounded-md">
-					<MapPin size={24} weight="fill" />
-					<span>Location</span>
+				<div className="flex gap-1 bg-purple-300 p-2 rounded-md text-purple-500">
+					<MapPin size={22} weight="fill" />
+					<span className="text-purple-700">Location</span>
 				</div>
-				<div className="bg-yellow-300 p-2 rounded-md">
-					<ShoppingCart size={24} weight="fill" />
+				<div className="bg-yellow-300 p-2 rounded-md text-yellow-700">
+					<ShoppingCart size={22} weight="fill" />
 				</div>
 			</nav>
 		</header>
